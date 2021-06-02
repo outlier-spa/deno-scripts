bbd161b12f0ab9c4053483e68648852ffa8210fd
jpenailillo <jpenailillo@outlier.cl>
2021-06-02 16:36:23 -0400

    max-pages

--- github/issues/delete.ts
+++ github/issues/delete.ts
@@ -8,15 +8,15 @@ const headers = {
 	authorization: `bearer ${token}`
 };
 
-async function getIssuesPage(page: number = 1): Promise<Array<any>> {
+async function getIssuesPage(page: number = 1, maxPages: number = Infinity): Promise<Array<any>> {
 	const url = `https://api.github.com/repos/${org}/${repo}/issues?state=all&per_page=100&page=${page}`;
 	const result = await (await fetch(url, { headers})).json();
-	if (result.length === 100) return result.concat(await getIssuesPage(page+1));
+	if (result.length === 100 && page < maxPages) return result.concat(await getIssuesPage(page+1, maxPages));
 	return result;
 }
 
 if (!org || !repo || !token){
-	console.error('param cannot be emty\nuse:\n--org org-name --repo repo-name --token bearer-token');
+	console.error('param cannot be empty\nuse:\n--org org-name --repo repo-name --token bearer-token');
 } else {
 	const result = await (getIssuesPage());
 	const issues = result.filter((issue: any) => !issue.pull_request)
