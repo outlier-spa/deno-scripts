9eb9dd6175acb67606605a7c0b5f6ba89220f915
jpenailillo <jpenailillo@outlier.cl>
2021-07-14 10:20:17 -0400

    repos

--- github/repositories/list.ts
+++ github/repositories/list.ts
@@ -3,21 +3,29 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 
 const params = parse(Deno.args);
 
-const { org, token } = params;
+const { org, token } = params;const headers = {
+	contentType: 'application/json',
+	authorization: `bearer ${token}`
+};
+
+async function getReposPage(page: number = 1, maxPages: number = Infinity): Promise<any> {
+	const url = `https://api.github.com/orgs/${org}/repos?per_page=100&page=${page}`;
+	const result = await (await fetch(url, { headers })).json();
+	if (result.length === 100 && page < maxPages) return result.concat(await getReposPage(page+1, maxPages));
+	return result;
+}
 
 if (!token || !org) {
 	console.error(`param cannot be empty\nuse:\n--org org-name --token bearer-token`);
 } else {
-	const headers = {
-		contentType: 'application/json',
-		authorization: `bearer ${token}`
-	};
+
 
 	const repos = await (await fetch(`https://api.github.com/orgs/${org}/repos?per_page=100`, { headers })).json();
 
 	if (!Array.isArray(repos)){
 		console.error(`Token || organization is invalid.`)
 	} else {
+		const repos = await getReposPage();
 		const file = await Deno.open("./repositories.csv", { write: true, create: true, truncate: true });
 		let rows = [['name', 'fullname']];
 		repos.forEach((repo: any) => {
