46fcd7a3529217f57fddf7321fca14c1bae0524e
Juliuz <jpenailillo@outlier.cl>
2022-06-08 17:34:05 -0400

    cron

--- cron/cron.ts
+++ cron/cron.ts
@@ -2,7 +2,7 @@ import {cron} from 'https://deno.land/x/deno_cron/cron.ts';
 
 Deno.create('./cron.txt');
 
-cron('1 * * * * *', () => {
+cron('* * * * * *', () => {
 	const file = Deno.openSync('./cron.txt', { write: true, append: true });
 	file.writeSync(new TextEncoder().encode(`${new Date()}\n`));
 	file.close();
